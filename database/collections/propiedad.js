80157d3b2ff4669c7a70d7d12691e33c96ec5116
ValeriaSandy <valita641selene@gmail.com>
2018-06-01 20:14:22 -0400

    propiedad

---
+++ database/collections/propiedad.js
@@ -0,0 +1,26 @@
+const mongoose = require("../connect");
+var userSchema = {
+  Ciudad : String,
+  Zona : String,
+  Direccion : String,
+  Precio: String,
+  Fecha_de_Entrega: String,
+  Fecha_de_Publicacion: String,
+  Año_de_Construccion: String,
+  Superficie_de_Terreno: String,
+  Superficie_Construida: String,
+  Servicios: String,
+  Numero_de_Baños: Number,
+  Pisos: Number,
+  Elevedor:String,
+  Piscina :String,
+  Parqueos: String,
+  Numero_de_Parqueos : Number,
+  Latitud: String,
+  Escuelas_Cercanas: String,
+  Descripcion: String,
+
+
+};
+var propiedad = mongoose.model("propiedad", userSchema);
+module.exports = propiedad;
--- routes/api/v1.0/services.js
+++ routes/api/v1.0/services.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var Vendedor = require("../../../database/collections/vendedor");
+var Propiedad = require("../../../database/collections/propiedad");
 //creacion
 
 router.post("/vendedor", (req, res) => {
@@ -60,5 +61,75 @@ router.get(/vendedor\/[a-z0-9]{24,24}$/, (req, res) => {
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post("/propiedad", (req, res) => {
+  if(req.body.Ciudad == " " && req.body.Fecha_de_Entrega== " " ){
 
+    res.status(400).json({
+      "msn": "formato incorrecto"
+    })
+    return;
+  }
+
+var propiedad = {
+
+  Ciudad : req.body.Ciudad,
+  Zona : req.body.Zona,
+  Direccion : req.body.Direccion,
+  Precio: req.body.Precio,
+  Fecha_de_Entrega: req.body.Fecha_de_Entrega,
+  Fecha_de_Publicacion: req.body.Fecha_de_Publicacion,
+  Año_de_Construccion: req.body.Año_de_Construccion,
+  Superficie_de_Terreno: req.body.Superficie_de_Terreno,
+  Superficie_Construida: req.body.Superficie_de_Construida,
+  Servicios: req.body.Servicios,
+  Numero_de_Baños: req.body.Numero_de_Baños,
+  Pisos: req.body.Pisos,
+  Elevedor:req.body.Elevedor,
+  Piscina :req.body.Piscina,
+  Parqueos: req.body.Parqueos,
+  Numero_de_Parqueos : req.body.Numero_de_Parqueos,
+  Latitud: req.body.Latitud,
+  Escuelas_Cercanas: req.body.Escuelas_cercanas,
+  Descripcion: req.body.Descripcion,
+};
+
+var propiedadData = new Propiedad(propiedad);
+propiedadData.save().then( () =>{
+  res.status(200).json({
+    "msn": "propiedad registrado"
+  });
+});
+});
+//read
+router.get("/propiedad",(req, res, next) => {
+  Propiedad.find({}).exec((error, docs) => {
+    res.status(200).json(docs);
+  })
+});
+//read
+router.get(/propiedad\/[a-z0-9]{24,24}$/, (req, res) => {
+  var url = req.url;
+  var id = url.split("/")[2];
+  Propiedad.findOne({_id : id}).exec((error, docs) => {
+    if  (docs != null) {
+      res.status(200).json(docs);
+      return;
+    }
+    res.status(200).json({
+      "msm" : "No existe el recurso"
+    });
+  })
+});
+router.get(/propiedad\/[a-z0-9]{24,24}$/, (req, res) => {
+  var url = req.url;
+  var id = url.split("/")[2];
+  Propiedad.findOne({_id : id}).remove().exec((error, docs) => {
+    res.status(200).json(docs);
+  });
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 module.exports = router;
