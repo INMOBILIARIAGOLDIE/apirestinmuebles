07cdf9ebf5b4a489184ee538285029384a972162
ValeriaSandy <valita641selene@gmail.com>
2018-07-06 11:43:01 -0400

    home

--- database/collections/homes.js
+++ database/collections/homes.js
@@ -2,23 +2,22 @@ const mongoose = require("../connect");
 var mon = require('mongoose');
 var Schema = mon.Schema;
 var homeSchema = new Schema({
-  Ciudad : String,
-  Tipo : String,
-  Estado : String,
-  Direccion : String,
-  Precio: String,
-  Año_de_Construccion: String,
-  Superficie_de_Terreno: String,
-  Numero_de_Cuartos:Number,
-  Numero_de_Baños: Number,
-  Latitud: Number,
-  Longitud: Number,
-  Vecindario: String,
-  Descripcion: String,
-  Gallery: Array,
-  Numero_de_Contacto: Number,
-  date:{type:Date,default:Date.now()}
-
+  city: String,
+  tipo: String,
+  estado :String,
+  cuartos : Number,
+  baños: Number,
+  superficie : Number,
+  antiguedad : Number,
+  street : String,
+  descripcion : String,
+  price : Number,
+  lat : Number,
+  lon : Number,
+  neighborhood : String,
+  gallery : Array,
+  contact : Number,
+  date: {type:Date,default:Date.now()}
 });
 var home = mongoose.model("homes", homeSchema);
 module.exports = home;
