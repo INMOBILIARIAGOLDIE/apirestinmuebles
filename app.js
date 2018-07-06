663d515c82970be5d5d1a90ccc6e8cb6c2b6a54c
ValeriaSandy <valita641selene@gmail.com>
2018-07-06 04:49:44 -0400

    modificando la ip en el app.js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var createError = require('http-errors');
+const IPWIFI = require('./database/collections/IP')
+
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -9,6 +12,8 @@ var service = require('./routes/api/v1.0/services');
 
 
 var app = express();
+const port = process.env.PORT || 4030
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,26 +26,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/api/v1.0/', service);
+app.use('/api/v1.0/', service)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
-var port = 7777;
 app.listen(port, () => {
-  console.log("server running in " + port);
-});
+    console.log(`Api-rest inmueble corriendo en ${IPWIFI}:${port}`)
+})
+module.exports = app;
